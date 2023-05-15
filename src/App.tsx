1450a40efdee66fe472693e67a66ca00dfd7bc5d
SamNi <1205470772@qq.com>
2023-05-15 19:03:32 +0800

    add axios and IM

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from "antd";
+import axios from 'axios';
+import { TUIKit } from '@tencentcloud/chat-uikit-react';
 
 function App() {
   return (
