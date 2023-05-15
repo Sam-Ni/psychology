a41f3be89b53a226c5e6bc2a21615f45f5821991
SamNi <1205470772@qq.com>
2023-05-15 19:16:40 +0800

    add axios and IM

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from "antd";
+import {Button, Input} from "antd";
+import { Form } from 'antd';
 import axios from 'axios';
 import { TUIKit } from '@tencentcloud/chat-uikit-react';
 
 function App() {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <Button type={'primary'}>Button</Button>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </header>
     </div>
   );
