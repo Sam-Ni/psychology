09de243ddb7c719cd210d3b3d903d2fae4a2d6f3
dealimbo <3020747055@qq.com>
2023-05-23 22:21:24 +0800

    修改日历组件

--- src/components/home/userhome/scheduling-calendar/scheduling-calendar.tsx
+++ src/components/home/userhome/scheduling-calendar/scheduling-calendar.tsx
@@ -1,14 +1,18 @@
 import React, {useState} from 'react';
-import {Calendar, Card, Col, Typography} from "antd";
-import type { Dayjs } from 'dayjs';
+import {Button, Calendar, Card, Col, Space, Typography} from "antd";
+import dayjs, { Dayjs } from 'dayjs';
 import {getEngagement} from "../../../../data/fake-data";
 import type { CellRenderInfo } from 'rc-picker/lib/interface';
-import './scheduling-calendar.css'
+import './scheduling-calendar.css';
+import {LeftOutlined, RightOutlined} from '@ant-design/icons';
 
 function SchedulingCalendar() {
+  //当前日期
+  const [selectedDate, setSelectedDate] = useState<Dayjs>(dayjs());
+
   const dateCellRender = (value: Dayjs) => {
     return (
-      getEngagement(value) ? <h5>有排班</h5> : null
+      getEngagement(value) ? <h5>有排班</h5> : <h5><br/></h5>
     );
   };
 
@@ -21,13 +25,19 @@ function SchedulingCalendar() {
     <Card>
       <Col>
         <Calendar className={'my-calendar'}
+          value={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
           fullscreen={true}
           cellRender={cellRender}
           headerRender={({value, type, onChange, onTypeChange}) => {
             return (
-              <div style={{padding: 8}}>
-                <Typography.Title level={4}>{getDate()}</Typography.Title>
-              </div>
+              <Space style={{padding: 8, justifyContent: 'space-between', width: '100%'}} size={"middle"} >
+                <div className={'calendar-title'}>{getTitle(value)}</div>
+                <Space>
+                  <Button icon={<LeftOutlined />} size={"small"} onClick={() => setSelectedDate(selectedDate.subtract(1, 'month'))}/>
+                  <Button icon={<RightOutlined />} size={"small"} onClick={() => setSelectedDate(selectedDate.add(1, 'month'))}/>
+                </Space>
+              </Space>
             )
           }}
         />
@@ -36,9 +46,9 @@ function SchedulingCalendar() {
   );
 }
 
-function getDate() {
-  const today = new Date();
-  return today.getFullYear() + "年" + (today.getMonth() + 1) + '月';
+function getTitle(value:Dayjs) {
+  const selectDate = value.toDate();
+  return selectDate.getFullYear() + "年" + (selectDate.getMonth() + 1) + '月';
 }
 
 export default SchedulingCalendar;
